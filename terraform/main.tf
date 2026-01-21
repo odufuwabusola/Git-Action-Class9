020cb8eb4adf8a33892258ea5c83b2c6462fd289
BUSOLA-O <102264029+BUSOLA-O@users.noreply.github.com>
2026-01-21 09:08:03 +0000

    terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,191 @@
+terraform {
+
+
+
+  backend "s3" {
+    bucket  = "teachbleat-cicd-state-buckets"
+    key     = "envs/dev/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+#####################
+# DATA
+#####################
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+#####################
+# VPC
+#####################
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = { Name = "WeeklyProject-VPC" }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+#####################
+# SUBNETS
+#####################
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet1_cidr
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet2_cidr
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+}
+
+
+
+#####################
+# ROUTING
+#####################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# NAT
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.this]
+}
+
+
+#####################
+# SECURITY GROUPS
+#####################
+resource "aws_security_group" "public_web" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "public_app" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+
+#####################
+# EC2 INSTANCES
+#####################
+resource "aws_instance" "Nginx" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.public_web.id]
+  key_name               = var.key_pair_name
+
+  tags = {
+    Name = "Nginx_Node"
+  }
+
+}
+
+resource "aws_instance" "Java" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_2.id
+  vpc_security_group_ids = [aws_security_group.public_app.id]
+  key_name               = var.key_pair_name
+
+  tags = {
+    Name = "Java_Node"
+  }
+
+}
+
+resource "aws_instance" "Python" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_2.id
+  vpc_security_group_ids = [aws_security_group.public_app.id]
+  key_name               = var.key_pair_name
+
+  tags = {
+    Name = "Python_Node"
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet1_cidr" {
+  description = "CIDR block for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet2_cidr" {
+  description = "CIDR block for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+
+variable "key_pair_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
